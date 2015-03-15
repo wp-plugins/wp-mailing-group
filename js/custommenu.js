dd8f8cb213657909a1170fb5b682e5aa85d91ea9
marcusbs <marcusbs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-15 23:46:41 +0000

    Initial Release
    git-svn-id: https://plugins.svn.wordpress.org/wp-mailing-group/trunk@1113536 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/custommenu.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('#toplevel_page_mailinggroup_list').removeClass("wp-not-current-submenu");
-	jQuery('#toplevel_page_mailinggroup_list').addClass("wp-has-current-submenu wp-menu-open");
-	jQuery('#adminmenu').find("#toplevel_page_mailinggroup_list > a:first-child").removeClass("wp-not-current-submenu");
-	jQuery('#adminmenu').find("#toplevel_page_mailinggroup_list > a:first-child").addClass("wp-has-current-submenu");
-});
